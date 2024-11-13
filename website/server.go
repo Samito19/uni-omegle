73cee5fd434c43018054c349182cb395ba04761c
Sami Amsaf <samiamsaf1902@gmail.com>
2024-11-13 11:13:52 -0500

    Implemented a basic logging middleware service with no persistence

--- website/server.go
+++ website/server.go
@@ -10,16 +10,25 @@ func handlePing(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "pong")
 }
 
+func LoggingMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Println(fmt.Sprintf("%v %v %v %v", r.RemoteAddr, r.Method, r.URL, r.UserAgent()))
+		next.ServeHTTP(w, r)
+	})
+}
+
 func main() {
+	fmt.Println("Server is listening at port 3000...")
+	mux := http.NewServeMux()
+
 	// File server for the website's statis files
 	fs := http.FileServer(http.Dir("./website/static"))
 	if fs == nil {
 		log.Fatal("Failed to create file server")
 	}
-	http.Handle("/", fs)
 
-	http.HandleFunc("/ping", handlePing)
+	mux.Handle("/", LoggingMiddleware(fs))
+	mux.HandleFunc("/ping", handlePing)
 
-	fmt.Println("Server is listening at port 3000...")
-	log.Fatal(http.ListenAndServe(":3000", nil))
+	log.Fatal(http.ListenAndServe(":3000", LoggingMiddleware(mux)))
 }
